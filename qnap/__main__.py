870dd535130897b4bea55a5b3d83f2470390e500
James Jhang <e8315402@gmail.com>
2022-04-07 16:59:11 +0800

    support glob, fullpath, and api status checker

--- qnap/__main__.py
+++ qnap/__main__.py
@@ -3,10 +3,84 @@ import os
 
 from .filestation import FileStation
 
+# from rich.traceback import install
+# install(show_locals=True)
 
 def concat_path(*paths):
     paths = map(lambda p: os.path.normpath(p), paths)
-    return os.path.join(*paths).replace('\\', '/')
+    return os.path.join(*paths)
+
+def upload(filestation: FileStation, upload_object, target_dir, fullpath, overwrite):
+    target_path = list(filter(lambda p: p != '', os.path.normpath(target_dir).split(os.sep)))
+    current_dir = target_path[0]
+    for target_folder in target_path[1:]:
+        remote_folders = filestation.list_folders(current_dir, nameonly=True)
+        if target_folder not in remote_folders:
+            filestation.create_folder(
+                current_dir,
+                target_folder
+            )
+            print(f'Created target folder: {concat_path(current_dir, target_folder)}')
+        current_dir = concat_path(current_dir, target_folder)
+
+    if fullpath:
+        print('fullpath is set. Create path folders...')
+        if os.path.isdir(upload_object):
+            path = upload_object
+        else:
+            path = os.path.dirname(upload_object)
+        upload_object_path = filter(lambda p: p != '', path.split(os.sep))
+        current_dir = target_dir
+        for folder in upload_object_path:
+            remote_folders = filestation.list_folders(current_dir, nameonly=True)
+            if folder not in remote_folders:
+                filestation.create_folder(
+                    current_dir,
+                    folder
+                )
+                print(f'Created folder: {concat_path(current_dir, folder)}')
+            else:
+                print(f'Folder already exists: {concat_path(current_dir, folder)}')
+            current_dir = concat_path(current_dir, folder)
+
+    if os.path.isdir(upload_object):
+        print('Upload object is a folder. Upload all objects in the folder.')
+        if not fullpath:
+            # if fullpath is not set, we need to check the upload_object folder is created or not;
+            # otherwise, since the upload_object folder is created already, we do nothing.
+            folders = filestation.list_folders(target_dir, nameonly=True)
+            root_dir = os.path.basename(upload_object)
+            if root_dir not in folders:
+                filestation.create_folder(
+                    target_dir,
+                    root_dir
+                )
+                print(f'Created folder: {concat_path(target_dir, root_dir)}')
+        for (dirpath, subfolders, filenames) in os.walk(upload_object):
+            target_folder = concat_path(target_dir, dirpath)
+            for subfolder in subfolders:
+                filestation.create_folder(
+                    target_folder,
+                    subfolder
+                )
+                print(f"Created subfolder: {concat_path(target_folder, subfolder)}")
+            for file in filenames:
+                print(f'Upload {file} to {target_folder}...')
+                with open(concat_path(dirpath, file), 'rb') as f:
+                    upload_file = concat_path(target_folder, file)
+                    filestation.upload(
+                        upload_file,
+                        f.read(),
+                        overwrite
+                    )
+    else: # upload_object is a file
+        with open(upload_object, 'rb') as f:
+            upload_file = concat_path(
+                target_dir,
+                os.path.basename(upload_object)
+            )
+            filestation.upload(upload_file, f.read(), overwrite)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Upload file/folder to QNAP QTS NAS.')
@@ -15,50 +89,30 @@ if __name__ == '__main__':
     parser.add_argument('password', type=str, metavar='pwd', help='Password')
     parser.add_argument('upload_object', type=str, metavar='upload', help='The file or folder to be uploaded.')
     parser.add_argument('target_dir', type=str, metavar='target', help='The target directory on the NAS.')
+    parser.add_argument('-g', '--glob', action='store_true', help='Whether use the glob module to find all the pathnames matching a specified pattern.')
     parser.add_argument('-o', '--overwrite', action='store_true', help='Whether overwrite the same name file/folder on the NAS.')
     parser.add_argument('-f', '--fullpath', action='store_true', help='Whether create the full path of the uploaded file on the NAS.')
     parser.set_defaults(overwrite=False)
     args = parser.parse_args()
 
-
     filestation = FileStation(args.host, args.user, args.password)
 
-    if os.path.isdir(args.upload_object):
-        items = filestation.list(args.target_dir)['datas']
-        folders = [it['filename'] for it in items if it['isfolder']]
-        root_dir = os.path.basename(args.upload_object)
-        if root_dir not in folders:
-            filestation.create_folder(
-                args.target_dir,
-                root_dir
+    if args.glob:
+        from glob import glob
+        matching_names = glob(args.upload_object)
+        for name in matching_names:
+            upload(
+                filestation,
+                upload_object=os.path.normpath(name),
+                target_dir=args.target_dir,
+                fullpath=True,
+                overwrite=args.overwrite
             )
-            print(f'Created folder: {concat_path(args.target_dir, root_dir)}')
-        for (dirpath, dirnames, filenames) in os.walk(args.upload_object):
-            norm_dirpath = concat_path(dirpath)
-            target_folder = concat_path(args.target_dir, norm_dirpath)
-            for folder in dirnames:
-                filestation.create_folder(
-                    target_folder,
-                    folder
-                )
-                print(f"Created subfolder: {concat_path(target_folder, folder)}")
-            for file in filenames:
-                print(f'Upload {file} ...')
-                with open(concat_path(norm_dirpath, file), 'rb') as f:
-                    upload_file = concat_path(target_folder, file)
-                    print(filestation.upload(
-                        upload_file,
-                        f.read(),
-                        args.overwrite
-                    ))
     else:
-        if args.fullpath:
-            # TODO
-            raise NotImplementedError()
-        else:
-            with open(args.upload_object, 'rb') as f:
-                upload_file = concat_path(
-                    args.target_dir,
-                    os.path.basename(args.upload_object)
-                )
-                filestation.upload(upload_file, f.read(), args.overwrite)
+        upload(
+            filestation,
+            upload_object=os.path.normpath(args.upload_object),
+            target_dir=args.target_dir,
+            fullpath=args.fullpath,
+            overwrite=args.overwrite
+        )
--- qnap/filestation.py
+++ qnap/filestation.py
@@ -11,26 +11,42 @@ class FileStation(Qnap):
         """
         List all shared folders.
         """
-        return self.req(self.endpoint(
-            func='get_tree',
-            params={
-                'is_iso': 0,
-                'node': 'share_root',
-            }
-        ))
+        return self.status_checker(
+            self.req(self.endpoint(
+                func='get_tree',
+                params={
+                    'is_iso': 0,
+                    'node': 'share_root',
+                }
+            ))
+        )
 
     def list(self, path, limit=10000):
         """
         List files in a folder.
         """
-        return self.req(self.endpoint(
-            func='get_list',
-            params={
-                'is_iso': 0,
-                'limit': limit,
-                'path': path
-            }
-        ))
+        return self.status_checker(
+            self.req(self.endpoint(
+                func='get_list',
+                params={
+                    'is_iso': 0,
+                    'limit': limit,
+                    'path': path.replace('\\', '/')
+                }
+            ))
+        )
+
+    def list_folders(self, path, nameonly=False):
+        items = self.list(path.replace('\\', '/'))
+        items = items['datas']
+        folders = []
+        for it in items:
+            if it['isfolder']:
+                if nameonly:
+                    folders.append(it['filename'])
+                else:
+                    folders.append(it)
+        return folders
 
     def get_file_info(self, path):
         """
@@ -38,27 +54,31 @@ class FileStation(Qnap):
         """
         dir_path = os.path.dirname(path)
         file_name = os.path.basename(path)
-        return self.req(self.endpoint(
-            func='stat',
-            params={
-                'path': dir_path,
-                'file_name': file_name
-            }
-        ))
+        return self.status_checker(
+            self.req(self.endpoint(
+                func='stat',
+                params={
+                    'path': dir_path.replace('\\', '/'),
+                    'file_name': file_name
+                }
+            ))
+        )
 
     def search(self, path, pattern):
         """
         Search for files/folders.
         """
-        return self.req(self.endpoint(
-            func='search',
-            params={
-                'limit': 10000,
-                'start': 0,
-                'source_path': path,
-                'keyword': pattern
-            }
-        ))
+        return self.status_checker(
+            self.req(self.endpoint(
+                func='search',
+                params={
+                    'limit': 10000,
+                    'start': 0,
+                    'source_path': path.replace('\\', '/'),
+                    'keyword': pattern
+                }
+            ))
+        )
 
     def delete(self, path):
         """
@@ -66,14 +86,16 @@ class FileStation(Qnap):
         """
         dir_path = os.path.dirname(path)
         file_name = os.path.basename(path)
-        return self.req(self.endpoint(
-            func='delete',
-            params={
-                'path': dir_path,
-                'file_total': 1,
-                'file_name': file_name
-            }
-        ))
+        return self.status_checker(
+            self.req(self.endpoint(
+                func='delete',
+                params={
+                    'path': dir_path.replace('\\', '/'),
+                    'file_total': 1,
+                    'file_name': file_name
+                }
+            ))
+        )
 
     def download(self, path):
         """
@@ -81,27 +103,31 @@ class FileStation(Qnap):
         """
         dir_path = os.path.dirname(path)
         file_name = os.path.basename(path)
-        return self.req_binary(self.endpoint(
-            func='download',
-            params={
-                'isfolder': 0,
-                'source_total': 1,
-                'source_path': dir_path,
-                'source_file': file_name
-            }
-        ))
+        return self.status_checker(
+            self.req_binary(self.endpoint(
+                func='download',
+                params={
+                    'isfolder': 0,
+                    'source_total': 1,
+                    'source_path': dir_path.replace('\\', '/'),
+                    'source_file': file_name
+                }
+            ))
+        )
 
     def create_folder(self, path, name):
         """
         Create folder.
         """
-        return self.req_post(self.endpoint(
-            func='createdir'
-            ),
-            data={
-                'dest_path': path,
-                'dest_folder': name,
-            }
+        return self.status_checker(
+            self.req_post(self.endpoint(
+                func='createdir'
+                ),
+                data={
+                    'dest_path': path.replace('\\', '/'),
+                    'dest_folder': name,
+                }
+            )
         )
 
     def upload(self, path, data, overwrite=True):
@@ -111,19 +137,27 @@ class FileStation(Qnap):
         dir_path = os.path.dirname(path)
         file_path = path.replace('/', '-')
         file_name = os.path.basename(path)
-        return self.req_post(self.endpoint(
-            func='upload',
-            params={
-                'type': 'standard',
-                'overwrite': 1 if overwrite else 0,
-                'dest_path': dir_path,
-                'progress': file_path
-            }),
-            files={
-                'file': (
-                    file_name,
-                    data,
-                    'application/octet-stream'
-                )
-            }
+        return self.status_checker(
+            self.req_post(self.endpoint(
+                func='upload',
+                params={
+                    'type': 'standard',
+                    'overwrite': 1 if overwrite else 0,
+                    'dest_path': dir_path.replace('\\', '/'),
+                    'progress': file_path
+                }),
+                files={
+                    'file': (
+                        file_name,
+                        data,
+                        'application/octet-stream'
+                    )
+                }
+            )
         )
+
+    def status_checker(self, resp):
+        if 'success' in resp:
+            if resp['success'] != 'true' or resp['status'] != 1:
+                raise Exception(str(resp))
+        return resp
